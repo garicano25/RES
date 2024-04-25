f122fd62494ddeb925816a55ad2240bcfb9f7a29
garicano25 <garicano25@gmail.com>
2024-04-25 10:16:59 -0600

    1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id('ID_USUARIO');
+            $table->string('NOMBRE');
+            $table->string('APELLIDO_PATERNO');
+            $table->string('APELLIDO_MATERNO');
+            $table->string('DIRECCION');
+            $table->string('TELEFONO');
+            $table->string('FECHA_NACIMIENTO');
+            $table->string('FOTO');
+            $table->string('CORREO')->unique();
+            $table->string('PASSWORD');
+            $table->integer('TIPO_USUARIO_ID');
+            $table->integer('CARGO_USUARIO_ID'); 
+            $table->boolean('ACTIVO')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
