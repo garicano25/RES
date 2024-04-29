1da0b76741f006786627e6b1cedeb6953914e68c
andres69542 <andres69542@gmail.com>
2024-04-29 17:54:39 -0600

    CONTROLLER AREA

--- app/Http/Controllers/organizacion/AreaController.php
+++ app/Http/Controllers/organizacion/AreaController.php
@@ -9,5 +9,24 @@ use Illuminate\Http\Request;
 
 class AreaController extends Controller
 {
-    //
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ID_AREA' => 'required',
+            'NOMBRE' => 'required',
+            'DESCRIPCION' => 'required',
+            'TIPO_AREA_ID' => 'required',
+            'USUARIO_ID' => 'required',
+            'ACTIVO' => 'required|image',
+        ]);
+
+
+
+        
+        
+    }
+
+
+
 }
