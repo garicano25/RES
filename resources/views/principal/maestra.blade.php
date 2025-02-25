38555cc93f81d210349ab0d5625054944d1f3349
andres69542 <andres69542@gmail.com>
2025-02-25 17:39:28 -0600

    CAMBIOS

--- resources/views/principal/maestra.blade.php
+++ resources/views/principal/maestra.blade.php
@@ -501,7 +501,7 @@
 
 
     @if(request()->is('Selección'))
-    <script src="/assets/js_sitio/seleccion/seleccion.js?v=19.1"></script>
+    <script src="/assets/js_sitio/seleccion/seleccion.js?v=19.2"></script>
     @endif
 
 
