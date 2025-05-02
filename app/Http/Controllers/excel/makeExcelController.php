0ddd44327d0f47b98525e71544da022ed6f48aac
andres69542 <andres69542@gmail.com>
2025-05-02 12:50:30 -0600

    Update makeExcelController.php

--- app/Http/Controllers/excel/makeExcelController.php
+++ app/Http/Controllers/excel/makeExcelController.php
@@ -1227,7 +1227,7 @@ class makeExcelController extends Controller{
 
 
 
-            $jerarquia = catalogojerarquiaModel::whereIn('ID_CATALOGO_JERARQUIA', $val->NIVEL_JERARQUICO_DPT)->pluck('NOMBRE_JERARQUIA');
+            $jerarquia = catalogojerarquiaModel::where('ID_CATALOGO_JERARQUIA', $val->NIVEL_JERARQUICO_DPT)->pluck('NOMBRE_JERARQUIA');
 
             // Empezar por la celda B29
             $startRow1 = 26;
