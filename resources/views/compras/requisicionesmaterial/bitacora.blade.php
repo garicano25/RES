97ad1fcb6d2f57aaaf236e10b0d0f4852dd65c6f
andres69542 <andres69542@gmail.com>
2025-06-28 11:23:29 -0600

    Update bitacora.blade.php

--- resources/views/compras/requisicionesmaterial/bitacora.blade.php
+++ resources/views/compras/requisicionesmaterial/bitacora.blade.php
@@ -643,7 +643,7 @@
                                                                     <label class="form-label fw-bold">Forma de pago:</label>
                                                                     <select class="form-select forma-pago" name="FORMA_PAGO[]">
                                                                         <option value="">Seleccionar forma de pago</option>
-                                                                        <option value="1">Transferencia bancaria</option>
+                                                                        <option value="1">Transferencia bancaria (Pago anticipado)</option>
                                                                         <option value="5">Efectivo (caja chica)</option>
                                                                         <option value="3">Tarjeta de crédito</option>
                                                                         <option value="4">Tarjeta de débito</option>
