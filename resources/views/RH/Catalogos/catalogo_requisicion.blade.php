602f63a0ffb78220281f147cadfe8883fbe4574f
andres69542 <andres69542@gmail.com>
2025-09-27 10:25:24 -0600

    Update catalogo_requisicion.blade.php

--- resources/views/RH/Catalogos/catalogo_requisicion.blade.php
+++ resources/views/RH/Catalogos/catalogo_requisicion.blade.php
@@ -35,7 +35,7 @@
             <div class="card h-100 text-center">
                 <div class="card-body d-flex flex-column justify-content-center align-items-center">
                     <h5 class="card-title mb-4">Catálogo motivo de la vacante</h5>
-                    <a class="btn btn-primary mt-3" href="{{ url('/m otivovacante') }}">Ver Catálogo</a>
+                    <a class="btn btn-primary mt-3" href="{{ url('/motivovacante') }}">Ver Catálogo</a>
                 </div>
             </div>
         </div>
