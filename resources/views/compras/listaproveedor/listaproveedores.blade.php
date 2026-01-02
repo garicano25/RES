4405cf8db0cecc7be9d992c690dc022caa6b819e
andres69542 <andres69542@gmail.com>
2026-01-02 09:21:50 -0600

    Update listaproveedores.blade.php

--- resources/views/compras/listaproveedor/listaproveedores.blade.php
+++ resources/views/compras/listaproveedor/listaproveedores.blade.php
@@ -675,7 +675,7 @@
                                 </div>
 
                                 <div class="col-12 mb-3">
-                                    <label>¡El proveedor realizará salidas de almacén!</label>
+                                    <label>¡El proveedor es interno!</label>
                                     <select class="form-control" name="TIENE_ASIGNACION" id="TIENE_ASIGNACION">
                                         <option value="" selected disabled>Seleccione una opción</option>
                                         <option value="1">Sí</option>
