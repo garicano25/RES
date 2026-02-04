872f043f27e949c79d7dbc4ce4484b45d4b1fb4a
andres69542 <andres69542@gmail.com>
2026-02-04 10:23:59 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1123,7 +1123,7 @@ Route::get('/notificaciones', [notificacionController::class, 'notificaciones'])
 Route::get('codigo-postal/{cp}', function ($cp) {
     Log::info('Consulta CP desde: ' . request()->ip() . ', User-Agent: ' . request()->header('User-Agent'));
 
-    //  $token = "a5ba768d-eeac-4c0f-b0be-202ef91df93c";
+     $token = "a5ba768d-eeac-4c0f-b0be-202ef91df93c";
     $url = "https://api.copomex.com/query/info_cp/{$cp}?type=simplified&token={$token}";
 
     try {
