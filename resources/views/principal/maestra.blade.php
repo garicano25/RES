270b5a61f08f659afa6b00654e1c23012a8c3987
andres69542 <andres69542@gmail.com>
2026-02-05 17:34:22 -0600

    Update maestra.blade.php

--- resources/views/principal/maestra.blade.php
+++ resources/views/principal/maestra.blade.php
@@ -774,7 +774,7 @@
     @endif
 
     @if(request()->is('contratacion'))
-    <script src="/assets/js_sitio/contratacion/contratacion.js?v=11.30  "></script>
+    <script src="/assets/js_sitio/contratacion/contratacion.js?v=11.30"></script>
     <!-- Form wizard -->
     <script src="/assets/plugins/form_wizard_steps_bootstrap/form_wizard_script.js"></script>
     @endif
