1fb58359b1b21ab93a1c862068e431985599c896
andres69542 <andres69542@gmail.com>
2026-02-20 12:47:26 -0600

    Update maestracompras.blade.php

--- resources/views/principal/maestracompras.blade.php
+++ resources/views/principal/maestracompras.blade.php
@@ -627,7 +627,7 @@
     @endif
 
     @if(request()->is('listaproveedores'))
-    <script src="/assets/js_sitio/proveedor/listaproveedor.js?v=1.20"></script>
+    <script src="/assets/js_sitio/proveedor/listaproveedor.js?v=2"></script>
     <script src="/assets/plugins/form_wizard_steps_bootstrap/form_wizard_script.js"></script>
     @endif
 
